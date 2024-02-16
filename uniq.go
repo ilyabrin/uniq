d0507f77fba1734234dbe13fdcd07e8300669161
Ilya Brin <464157+ilyabrin@users.noreply.github.com>
2024-02-16 16:47:54 +0300

    fix: unnecessary comments removed

--- uniq.go
+++ uniq.go
@@ -11,43 +11,6 @@ const (
 	numbers          = "0123456789"
 )
 
-// func generateRandomStringWithUpperCase(seed *rand.Rand, length, numUpperCase int) string {
-
-// 	result := make([]byte, length)
-// 	countUpperCase := 0 // counter for uppercase chars
-
-// 	// generate random string with lower case
-// 	for i := range result {
-// 		result[i] = letters[seed.Intn(len(letters))]
-// 	}
-
-// 	// change case only for lowercase letters
-// 	for i := 0; i < numUpperCase; i++ {
-// 		randIndex := seed.Intn(length)
-// 		if result[randIndex] >= 'a' && result[randIndex] <= 'z' {
-// 			result[randIndex] = uppercaseLetters[result[randIndex]-'a']
-// 		}
-// 	}
-
-// 	// count uppercase letters
-// 	for i := 0; i < len(result); i++ {
-// 		if result[i] >= 'A' && result[i] <= 'Z' {
-// 			countUpperCase++
-// 		}
-// 	}
-
-// 	// change lower case to upper case in place
-// 	for countUpperCase < numUpperCase {
-// 		randIndex := seed.Intn(length)
-// 		if result[randIndex] >= 'a' && result[randIndex] <= 'z' {
-// 			result[randIndex] = uppercaseLetters[result[randIndex]-'a']
-// 			countUpperCase++
-// 		}
-// 	}
-
-// 	return string(result)
-// }
-
 func generateRandomStringWithUpperCase(seed *rand.Rand, length, numUpperCase int) string {
 
 	if numUpperCase > length {
